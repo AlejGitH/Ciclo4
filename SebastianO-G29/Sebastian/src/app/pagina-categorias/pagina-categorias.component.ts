35829a7152640d2b2c8584025c97e2ecb40765cc
juan pablo quiñones ordoñez <soadj118@gmail.com>
2021-12-08 17:58:16 -0500

    nuevo

---
+++ SebastianO-G29/Sebastian/src/app/pagina-categorias/pagina-categorias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-categorias',
+  templateUrl: './pagina-categorias.component.html',
+  styleUrls: ['./pagina-categorias.component.css']
+})
+export class PaginaCategoriasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
